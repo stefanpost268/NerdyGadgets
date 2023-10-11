072713d81df7390d1e11da7ee6191530e4615fd3
stefanpost268 <97079064@st.deltion.nl>
2023-10-11 11:15:26 +0200

    US-23 ADD: sorting on name or price (ASC/DESC)

--- browse.php
+++ browse.php
@@ -3,12 +3,9 @@
 include __DIR__ . "/header.php";
 
 $ReturnableResult = null;
-$Sort = "SellPrice";
-        $SortName = "price_low_high";
 
 $AmountOfPages = 0;
 $queryBuildResult = "";
-
 
 if (isset($_GET['category_id'])) {
     $CategoryID = $_GET['category_id'];
@@ -24,20 +21,37 @@ if (isset($_GET['products_on_page'])) {
     $ProductsOnPage = 25;
     $_SESSION['products_on_page'] = 25;
 }
+
 if (isset($_GET['page_number'])) {
     $PageNumber = $_GET['page_number'];
 } else {
     $PageNumber = 0;
 }
 
-// code deel 1 van User story: Zoeken producten
-// <voeg hier de code in waarin de zoekcriteria worden opgebouwd>
+// ORDER BY
+if(!isset($_GET['order_by'])) {
+    $orderByLabel = "price-ASC";
+}
 
-
-
-// <einde van de code voor zoekcriteria>
-// einde code deel 1 van User story: Zoeken producten
-
+switch($_GET['order_by']) {
+    case "price-ASC":
+        $orderBy = "ASC";
+        $Sort = "SellPrice";
+        break;
+    case "price-DESC":
+        $orderBy = "DESC";
+        $Sort = "SellPrice";
+        break;
+    case "name-ASC":
+        $orderBy = "ASC";
+        $Sort = "StockItemName";
+        break;
+    case "name-DESC":
+        $orderBy = "DESC";
+        $Sort = "StockItemName";
+        break;
+        
+}
 
 $Offset = $PageNumber * $ProductsOnPage;
 
@@ -46,14 +60,6 @@ if ($CategoryID != "") {
     $queryBuildResult .= " AND ";
     }
 }
-
-// code deel 2 van User story: Zoeken producten
-// <voeg hier de code in waarin het zoekresultaat opgehaald wordt uit de database>
-
-
-
-// <einde van de code voor zoekresultaat>
-// einde deel 2 van User story: Zoeken producten
 
 if ($CategoryID !== "") {
 $Query = "
@@ -68,7 +74,7 @@ $Query = "
            JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
            WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
            GROUP BY StockItemID
-           ORDER BY " . $Sort . "
+           ORDER BY " . $Sort . " " . $orderBy . "
            LIMIT ? OFFSET ?";
 
     $Statement = mysqli_prepare($databaseConnection, $Query);
@@ -91,8 +97,6 @@ $amount = $Result[0];
 if (isset($amount)) {
     $AmountOfPages = ceil($amount["count(*)"] / $ProductsOnPage);
 }
-
-
     function getVoorraadTekst($actueleVoorraad) {
         if ($actueleVoorraad > 1000) {
             return "Ruime voorraad beschikbaar.";
@@ -105,91 +109,87 @@ if (isset($amount)) {
     }
 ?>
 
-<!-- code deel 3 van User story: Zoeken producten : de html -->
-<!-- de zoekbalk links op de pagina  -->
+<div class="d-flex">
+    <form id="sideBar">
+        <p id="sideBarTitle">Filteren</p>
 
-
-
-<!-- einde zoekresultaten die links van de zoekbalk staan -->
-<!-- einde code deel 3 van User story: Zoeken producten  -->
-
-<div id="ResultsArea" class="Browse">
-    <?php
-    if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
-        foreach ($ReturnableResult as $row) {
-            ?>
-            <!--  coderegel 1 van User story: bekijken producten  -->
-
-
-
-            <!-- einde coderegel 1 van User story: bekijken producten   -->
-                <div id="ProductFrame">
-                    <?php
-                    if (isset($row['ImagePath'])) { ?>
-                        <div class="ImgFrame"
-                             style="background-image: url('<?php print "Public/StockItemIMG/" . $row['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
-                    <?php } else if (isset($row['BackupImagePath'])) { ?>
-                        <div class="ImgFrame"
-                             style="background-image: url('<?php print "Public/StockGroupIMG/" . $row['BackupImagePath'] ?>'); background-size: cover;"></div>
-                    <?php }
-                    ?>
-
-                    <div id="StockItemFrameRight">
-                        <div class="CenterPriceLeftChild">
-                            <h1 class="StockItemPriceText"><?php print sprintf(" %0.2f", berekenVerkoopPrijs($row["RecommendedRetailPrice"], $row["TaxRate"])); ?></h1>
-                            <h6>Inclusief BTW </h6>
-                        </div>
-                    </div>
-                    <h1 class="StockItemID">Artikelnummer: <?php print $row["StockItemID"]; ?></h1>
-                    <p class="StockItemName"><?php print $row["StockItemName"]; ?></p>
-                    <p class="StockItemComments"><?php print $row["MarketingComments"]; ?></p>
-                    <h4 class="ItemQuantity"><?php print getVoorraadTekst($row["QuantityOnHand"]); ?></h4>
-                </div>
-            <!--  coderegel 2 van User story: bekijken producten  -->
-
-
-
-            <!--  einde coderegel 2 van User story: bekijken producten  -->
-        <?php } ?>
-
-        <form id="PageSelector">
-		
-<!-- code deel 4 van User story: Zoeken producten  -->
-
-
-
-<!-- einde code deel 4 van User story: Zoeken producten  -->
+        <div>
+            </label for="order_by">Sorteer op:</label>
+            <select name="order_by" id="order_by" onchange="this.form.submit()">
+                <option value="price-ASC" <?php print($_GET['order_by'] == "price-ASC" ? "selected" : ""); ?> >Prijs oplopend</option>
+                <option value="price-DESC" <?php print($_GET['order_by'] == "price-DESC" ? "selected" : ""); ?> >Prijs aflopend</option>
+                <option value="name-ASC" <?php print($_GET['order_by'] == "name-ASC" ? "selected" : ""); ?> >Naam oplopend</option>
+                <option value="name-DESC" <?php print($_GET['order_by'] == "name-DESC" ? "selected" : ""); ?> >Naam aflopend</option>
+            </select>
+            
             <input type="hidden" name="category_id" id="category_id" value="<?php if (isset($_GET['category_id'])) {
                 print ($_GET['category_id']);
             } ?>">
-            <input type="hidden" name="result_page_numbers" id="result_page_numbers"
-                   value="<?php print (isset($_GET['result_page_numbers'])) ? $_GET['result_page_numbers'] : "0"; ?>">
-            <input type="hidden" name="products_on_page" id="products_on_page"
-                   value="<?php print ($_SESSION['products_on_page']); ?>">
-
-            <?php
-            if ($AmountOfPages > 0) {
-                for ($i = 1; $i <= $AmountOfPages; $i++) {
-                    if ($PageNumber == ($i - 1)) {
+        </div>
+    </form>
+    <div id="ResultsArea" class="Browse">
+    
+        <?php
+        if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
+            foreach ($ReturnableResult as $row) {
+                ?>
+                    <div id="ProductFrame">
+                        <?php
+                        if (isset($row['ImagePath'])) { ?>
+                            <div class="ImgFrame"
+                                style="background-image: url('<?php print "Public/StockItemIMG/" . $row['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
+                        <?php } else if (isset($row['BackupImagePath'])) { ?>
+                            <div class="ImgFrame"
+                                style="background-image: url('<?php print "Public/StockGroupIMG/" . $row['BackupImagePath'] ?>'); background-size: cover;"></div>
+                        <?php }
                         ?>
-                        <div id="SelectedPage"><?php print $i; ?></div><?php
-                    } else { ?>
-                        <button id="page_number" class="PageNumber" value="<?php print($i - 1); ?>" type="submit"
-                                name="page_number"><?php print($i); ?></button>
-                    <?php }
+
+                        <div id="StockItemFrameRight">
+                            <div class="CenterPriceLeftChild">
+                                <h1 class="StockItemPriceText"><?php print sprintf(" %0.2f", berekenVerkoopPrijs($row["RecommendedRetailPrice"], $row["TaxRate"])); ?></h1>
+                                <h6>Inclusief BTW </h6>
+                            </div>
+                        </div>
+                        <h1 class="StockItemID">Artikelnummer: <?php print $row["StockItemID"]; ?></h1>
+                        <p class="StockItemName"><?php print $row["StockItemName"]; ?></p>
+                        <p class="StockItemComments"><?php print $row["MarketingComments"]; ?></p>
+                        <h4 class="ItemQuantity"><?php print getVoorraadTekst($row["QuantityOnHand"]); ?></h4>
+                    </div>
+            <?php } ?>
+
+            <form id="PageSelector">
+                <input type="hidden" name="category_id" id="category_id" value="<?php if (isset($_GET['category_id'])) {
+                    print ($_GET['category_id']);
+                } ?>">
+                <input type="hidden" name="result_page_numbers" id="result_page_numbers"
+                    value="<?php print (isset($_GET['result_page_numbers'])) ? $_GET['result_page_numbers'] : "0"; ?>">
+                <input type="hidden" name="products_on_page" id="products_on_page"
+                    value="<?php print ($_SESSION['products_on_page']); ?>">
+
+                <?php
+                if ($AmountOfPages > 0) {
+                    for ($i = 1; $i <= $AmountOfPages; $i++) {
+                        if ($PageNumber == ($i - 1)) {
+                            ?>
+                            <div id="SelectedPage"><?php print $i; ?></div><?php
+                        } else { ?>
+                            <button id="page_number" class="PageNumber" value="<?php print($i - 1); ?>" type="submit"
+                                    name="page_number"><?php print($i); ?></button>
+                        <?php }
+                    }
                 }
-            }
+                ?>
+            </form>
+            <?php
+        } else {
             ?>
-        </form>
-        <?php
-    } else {
+            <h2 id="NoSearchResults">
+                Yarr, er zijn geen resultaten gevonden.
+            </h2>
+            <?php
+        }
         ?>
-        <h2 id="NoSearchResults">
-            Yarr, er zijn geen resultaten gevonden.
-        </h2>
-        <?php
-    }
-    ?>
+    </div>
 </div>
 
 <?php
