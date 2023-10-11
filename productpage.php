55a0cf6d860d88b2f1899ec6779f7a554635a59a
thijsj-poggers <s1200360@student.windesheim.nl>
2023-10-11 08:39:11 +0200

    US-13 ADD: Default image when not found on product page

--- productpage.php
+++ productpage.php
@@ -1,8 +1,13 @@
 <!-- dit is het bestand dat wordt geladen zodra je naar de website gaat -->
 <?php
    include __DIR__ . "/header.php";
-
+   $imageUrl = getStockItemImage($_GET['id'], $databaseConnection);
    $item = getStockItem($_GET['id'], $databaseConnection);
+   if (count($imageUrl) > 0) {
+      $imagePath = $imageUrl[0]['ImagePath'];
+   } else {
+      $imagePath = null;
+   }
 ?>
 
 <div class="container">
@@ -15,6 +20,16 @@
          <p>Product prijs: €<?php print(round($item["SellPrice"], 2)); ?></p>
          <p><?php print($item["QuantityOnHand"]); ?></p>
       </div>
+      <div>
+      <div class="warning">
+         <img width='300px' height='300px' src="<?php 
+         if($imagePath != null) {
+            print("Public/StockItemIMG/" . $imagePath);
+         } else {
+            print("Public/StockGroupIMG/Toys.jpg");
+         }
+         
+         ?>" />
    </div>
 </div>
 
