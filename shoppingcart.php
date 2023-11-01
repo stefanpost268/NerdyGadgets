adef8e652fc621d5554df984d3bf419a5036eeb9
stefanpost268 <97079064@st.deltion.nl>
2023-11-01 12:17:15 +0100

    US-29 ADD: info card in shopping cart page

--- shoppingcart.php
+++ shoppingcart.php
@@ -19,6 +19,7 @@ if (isset($_POST['clear_session'])) {
 }
 
 $products = [];
+$totalPrice = 0;
 
 if (isset($_SESSION["shoppingcart"])) {
     foreach ($_SESSION["shoppingcart"] as $id => $amount) {
@@ -32,51 +33,64 @@ if (isset($_SESSION["shoppingcart"])) {
             "amount" => $amount,
             'subtotal' => $subtotal,
         ];
+
+        $totalPrice += $subtotal;
     }
 }
 ?>
 
-<div style="padding: 25px;">
-    <h1>Winkelwagen</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Artikel</th>
-                <th>Prijs</th>
-                <th>Aantal</th>
-                <th>Subtotaal</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php if (!empty($products)) : ?>
-            <?php foreach ($products as $product) : ?>
+<h1>Winkelwagen</h1>
+<div style="padding: 25px; display:flex;">
+    <div class="table-left">
+        <table>
+            <thead>
                 <tr>
-                    <td>
-                        <div style="display: flex; align-items: center;">
-                            <img width='100' src='<?php echo $product["image"]; ?>'>
-                            <div style="margin-left: 10px;">
-                                <?php 
-                                echo $product["item"]["StockItemName"];
-                                echo "<br>";
-                                echo "Article ID: " . $product["item"]["StockItemID"];
-                                ?>
-                            </div>
-                        </div>
-                    </td>
-                    <td>€ <?php echo number_format($product["item"]["SellPrice"], 2); ?></td>
-                    <td><?php echo $product["amount"]; ?></td>
-                    <td>€ <?php echo number_format($product["subtotal"], 2); ?></td>
+                    <th>Artikel</th>
+                    <th>Prijs</th>
+                    <th>Aantal</th>
+                    <th>Subtotaal</th>
                 </tr>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <tr>
-                <td colspan='2'>Shopping cart is empty</td>
-            </tr>
-        <?php endif; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php if (!empty($products)) : ?>
+                    <?php foreach ($products as $product) : ?>
+                        <tr>
+                            <td>
+                                <div style="display: flex; align-items: center;">
+                                    <img width='100' src='<?php echo $product["image"]; ?>'>
+                                    <div style="margin-left: 10px;">
+                                        <?php
+                                        echo $product["item"]["StockItemName"];
+                                        echo "<br>";
+                                        echo "Article ID: " . $product["item"]["StockItemID"];
+                                        ?>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>€ <?php echo number_format($product["item"]["SellPrice"], 2); ?></td>
+                            <td><?php echo $product["amount"]; ?></td>
+                            <td>€ <?php echo number_format($product["subtotal"], 2); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <td colspan='2'>Shopping cart is empty</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
 
-    <form method="post">
+        <form method="post">
         <button type="submit" name="clear_session">Clear Session</button>
-    </form>
+        </form>
+    </div>
+
+    <div class="card-right" style="width: 40%">
+        <div style="border-style: solid; border-color:blue">
+            <h2>Overzicht van je bestelling</h2>
+            <div style="border-style: solid; border-color:red">
+                <h3>Totaal: € <?php print($totalPrice); ?></h3>
+            </div>
+        </div>
+    </div>
 </div>
