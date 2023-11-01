16c982e941e6fbef1aad93f4a70f3808b83bc30c
stefanpost268 <97079064@st.deltion.nl>
2023-11-01 13:20:31 +0100

    US-29 ADD: Better styling to shopping cart

--- shoppingcart.php
+++ shoppingcart.php
@@ -39,58 +39,64 @@ if (isset($_SESSION["shoppingcart"])) {
 }
 ?>
 
-<h1>Winkelwagen</h1>
-<div style="padding: 25px; display:flex;">
-    <div class="table-left">
-        <table>
-            <thead>
-                <tr>
-                    <th>Artikel</th>
-                    <th>Prijs</th>
-                    <th>Aantal</th>
-                    <th>Subtotaal</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (!empty($products)) : ?>
-                    <?php foreach ($products as $product) : ?>
-                        <tr>
-                            <td>
-                                <div style="display: flex; align-items: center;">
-                                    <img width='100' src='<?php echo $product["image"]; ?>'>
-                                    <div style="margin-left: 10px;">
-                                        <?php
-                                        echo $product["item"]["StockItemName"];
-                                        echo "<br>";
-                                        echo "Article ID: " . $product["item"]["StockItemID"];
-                                        ?>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>€ <?php echo number_format($product["item"]["SellPrice"], 2); ?></td>
-                            <td><?php echo $product["amount"]; ?></td>
-                            <td>€ <?php echo number_format($product["subtotal"], 2); ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else : ?>
+<div class="container">
+<h1 class="pb-5">Winkelwagen</h1>
+    <div class="row">
+        <div class="col-lg-7">
+            <table class="table" style="color:white;">
+                <thead>
                     <tr>
-                        <td colspan='2'>Shopping cart is empty</td>
+                        <th>Artikel</th>
+                        <th>Prijs</th>
+                        <th>Aantal</th>
+                        <th>Subtotaal</th>
                     </tr>
-                <?php endif; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php if (!empty($products)) : ?>
+                        <?php foreach ($products as $product) : ?>
+                            <tr>
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <img width='100' src='<?php echo $product["image"]; ?>' class="img-thumbnail">
+                                        <div class="ml-3 color:red;">
+                                            <p><?php print($product["item"]["StockItemName"]); ?></p>
+                                            <p><?php  print("Article ID: " . $product["item"]["StockItemID"]); ?></p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>€<?php echo number_format($product["item"]["SellPrice"], 2); ?></td>
+                                <td><?php echo $product["amount"]; ?></td>
+                                <td>€ <?php echo number_format($product["subtotal"], 2); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <tr>
+                            <td colspan='4'>Winkelmand is leeg</td>
+                        </tr>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
 
-        
-    </div>
-
-    <div class="card-right" style="width: 40%">
-        <div style="border-style: solid; border-color:blue">
-            <h2>Overzicht van je bestelling</h2>
-            <div style="border-style: solid; border-color:red; margin:10px;">
-                <h3>Totaal: € <?php print($totalPrice); ?></h3>
-                <form method="post">
-                    <button type="submit" name="clear_session">Legen Winkelmand</button>
-                </form>
+        <div class="col-lg-5">
+            <div class="card" style="background-color: #23272e; border-style: solid; border-color: white;">
+                <div class="card-body">
+                    <h2>Overzicht van je bestelling</h2>
+                    <div style="border-style: solid; border-color: white; padding: 5px;">
+                        <h3>Totaal: € <?php print($totalPrice); ?></h3>
+                        <form method="post">
+                            <button
+                                class="btn btn-primary"
+                                style="width: 100%;"
+                                type="submit"
+                                name="clear_session"
+                            >
+                                Legen Winkelmand
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
