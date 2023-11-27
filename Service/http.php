87e87081e6a56e902cb335cafce6dae1c210ad84
stefanpost268 <97079064@st.deltion.nl>
2023-11-27 19:22:57 +0100

    US-53 ADD: HTTP service

---
+++ Service/http.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Service;
+
+class HTTP
+{
+    public static function get(string $url, array $headers = []): array
+    {
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($curl);
+        $info = curl_getinfo($curl);
+
+        var_dump($info['header_size']);
+
+        curl_close($curl);
+
+        return [
+            "response" => $response,
+            "info" => $info
+        ];
+    }
+
+    public static function post(string $url, array $headers = [], array $data = []): array
+    {
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $response = curl_exec($curl);
+        $info = curl_getinfo($curl);
+
+        curl_close($curl);
+
+        return [
+            "response" => $response,
+            "info" => $info
+        ];
+    }
+}
