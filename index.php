d0f1e4dff79f04a7957656958174ba4e67718403
stefanpost268 <97079064@st.deltion.nl>
2023-12-06 09:38:30 +0100

    US-97 IMP: formatted main page to tailwind

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
-<!-- dit is het bestand dat wordt geladen zodra je naar de website gaat -->
-<?php
-include __DIR__ . "/header.php";
-?>
-<div class="IndexStyle">
+<?php include __DIR__ . "/header.php"; ?>
+<div class="mx-auto max-w-7xl">
     <div class="col-11">
-        <div class="TextPrice">
+        <div class="mt-16 select-none">
             <a href="productPage.php?id=93">
-                <div class="TextMain">
+                <div class="text-white font-vortice-concept text-5xl uppercase font-bold w-3/5 ml-10">
                     "The Gu" red shirt XML tag t-shirt (Black) M
                 </div>
                 <ul id="ul-class-price">
-                    <li class="HomePagePrice">€30.95</li>
+                    <li class="text-5xl text-blue-600 font-vortice-concept uppercase font-bold ml-7">€30.95</li>
                 </ul>
+            </a>
         </div>
-        </a>
-        <div class="HomePageStockItemPicture"></div>
+        <div class="bg-cover bg-no-repeat w-96 h-96 ml-55p mt-neg30p" style="background-image: url('Public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png');"></div>
     </div>
 </div>
-<?php
-include __DIR__ . "/footer.php";
-?>
-
+<?php include __DIR__ . "/footer.php"; ?>
