7a447600a89cb79aab773c3e72a3a5995336f645
stefanpost268 <97079064@st.deltion.nl>
2023-12-17 21:44:21 +0100

    US-81 ADD: mysqli_stmt_bind_param & on duplicate update in CheckoutController::updateOrCreateUser

--- Controller/CheckoutController.php
+++ Controller/CheckoutController.php
@@ -171,40 +171,28 @@ class CheckoutController
      * @param array $formData
      * @return int returns id of user.
      */
-    private function updateOrCreateUser($databaseConnection, $formData): int
+    private function updateOrCreateUser(mysqli $databaseConnection, array $userData): int
     {
-        $query = "SELECT `id` FROM `User` WHERE `email` = ?;";
+        $query = "INSERT INTO `User` (
+            `email`,
+            `name`
+        ) VALUES 
+        (
+            ?,
+            ?
+        ) ON DUPLICATE KEY UPDATE 
+            `email` = VALUES(`email`),
+            `name` = VALUES(`name`);";
 
         $statement = mysqli_prepare($databaseConnection, $query);
-        mysqli_stmt_bind_param($statement, "s", $formData["email"]);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_bind_result($statement, $userId);
-        mysqli_stmt_fetch($statement);
+
+        mysqli_stmt_bind_param($statement, 'ss', $userData['email'], $userData['name']);
+
+        $success = mysqli_stmt_execute($statement);
+        $insertedOrUpdatedId = ($success) ? mysqli_insert_id($databaseConnection) : 0;
         mysqli_stmt_close($statement);
 
-        if ($userId === null) {
-            $query = "INSERT INTO `User` (
-                `name`,
-                `email`
-            ) VALUES 
-            (   
-                '" . $formData['name'] . "',
-                '" . $formData['email'] . "'
-            );";
-
-            $statement = mysqli_prepare($databaseConnection, $query);
-            $success = mysqli_stmt_execute($statement);
-            $userId = ($success) ? mysqli_insert_id($databaseConnection) : 0;
-            mysqli_stmt_close($statement);
-        } else {
-            $query = "UPDATE `User` SET `name` = '" . $formData['name'] . "' WHERE `id` = $userId;";
-
-            $statement = mysqli_prepare($databaseConnection, $query);
-            $success = mysqli_stmt_execute($statement);
-            mysqli_stmt_close($statement);
-        }
-
-        return $userId;
+        return $insertedOrUpdatedId;
     }
 
     private function bindProductsOnTransaction(int $databaseId, array $shoppingCart, $databaseConnection): bool
