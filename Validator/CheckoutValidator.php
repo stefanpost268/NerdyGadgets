9b709c5d11495b6c4680427d4ecf6d2af3c1225a
stefanpost268 <97079064@st.deltion.nl>
2023-12-19 10:24:18 +0100

    US-124 ADD: backend validation validatePostalCode for checkout

--- Validator/CheckoutValidator.php
+++ Validator/CheckoutValidator.php
@@ -10,7 +10,8 @@ class CheckoutValidator
         return [
             ...$this->productStockAvailable($products),
             ...$this->validateEmail($formData),
-            ...$this->validateHouseNumer($formData)
+            ...$this->validateHouseNumer($formData),
+            ...$this->validatePostalCode($formData)
         ];
     }
 
@@ -35,21 +36,32 @@ class CheckoutValidator
         return $errors;
     }
 
-    private function validateEmail(): array {
+    private function validateEmail(array $formData): array {
         $errors = [];
-        if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        if(!filter_var($formData["email"], FILTER_VALIDATE_EMAIL)) {
             $errors[] = "Email is niet geldig";
         }
         return $errors;
     }
 
-    private function validateHouseNumer(): array {
+    private function validateHouseNumer(array $formData): array {
         $errors = [];
-        if(!isset($_POST["housenr"]) || $_POST["housenr"] == "") {
+        if(!isset($formData["housenr"]) || $formData["housenr"] == "") {
             $errors[] = "Huisnummer is verplicht";
-        } else if(!preg_match("/^\s*\d{1,3}\s*[A-Za-z]{0,1}\s*$/", $_POST["housenr"])) {
+        } else if(!preg_match("/^\s*\d{1,3}\s*[A-Za-z]{0,1}\s*$/", $formData["housenr"])) {
             $errors[] = "Huisnummer is niet geldig";
         }
+        return $errors;
+    }
+
+    public function validatePostalCode(array $formData): array {
+        $errors = [];
+        if(!isset($formData["postalcode"]) || $formData["postalcode"] == "") {
+            $errors[] = 'Postcode is verplicht.';
+        } else if (!preg_match('/^\d{4} [A-Za-z]{2}$/', $formData["postalcode"])) {
+            $errors[] = "Voer een gelde postcode in. (Bijvoorbeeld: 8302 BB)";
+        }
+
         return $errors;
     }
 }
