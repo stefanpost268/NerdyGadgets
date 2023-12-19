491413b2f3d62c48e1472880394dd23b0c3c4225
stefanpost268 <97079064@st.deltion.nl>
2023-12-19 14:46:59 +0100

    US-134 ADD: migration to replace INNODB with the ARCHIVE engine on table coldroomtemperatures_archive

---
+++ Database/Migrations/2023_12_19_1435_drop_table_coldroomtemperatures_archive_old.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Gegenereerd op: 28 nov 2023 om 11:51
+-- Serverversie: 10.4.27-MariaDB
+-- PHP-versie: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nerdygadgets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Removes table `coldroomtemperatures_archive_old`
+-- Replaced by `coldroomtemperatures_archive` running ARCHIVE engine
+--
+
+DROP TABLE coldroomtemperatures_archive_old;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
